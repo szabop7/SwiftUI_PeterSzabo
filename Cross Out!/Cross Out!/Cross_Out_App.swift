e8bb03cdb9e06ac4ca2d0de6aa3b36797eb1c407
Peter Szabo <peter.szabo@student.technikum.be>
2021-10-17 22:25:05 +0100

    Project + ReadMe added

---
+++ Cross Out!/Cross Out!/Cross_Out_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cross_Out_App.swift
+//  Cross Out!
+//
+//  Created by user207082 on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct Cross_Out_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SwiftUI_PeterSzabo
+
+This App is created by: Peter Szabo
+
+E-Mail: peter.szabo@student.hogent.com
+
+Enjoy using my App
